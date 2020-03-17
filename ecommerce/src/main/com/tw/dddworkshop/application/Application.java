6ead4dcba2049ecc5047b99f9164c201791ded63
Sandeep Jagtap <sandeepjagtap@Sandeeps-MacBook-Pro.local>
2020-03-17 15:27:37 +0530

    [Sandeep] Implemented Use case 1

--- ecommerce/src/main/com/tw/dddworkshop/application/Application.java
+++ ecommerce/src/main/com/tw/dddworkshop/application/Application.java
@@ -1,8 +1,13 @@
 package com.tw.dddworkshop.application;
 
+import com.tw.dddworkshop.domain.Cart;
+import com.tw.dddworkshop.domain.Product;
+
 public class Application {
 
     public static void main(String[] args) {
-
+        Cart cart = new Cart();
+        Product product = new Product("Ipad Pro");
+        cart.addProduct(product);
     }
 }
--- ecommerce/src/main/com/tw/dddworkshop/domain/Cart.java
+++ ecommerce/src/main/com/tw/dddworkshop/domain/Cart.java
@@ -1,6 +1,14 @@
 package com.tw.dddworkshop.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
 
+    List<Product> productList = new ArrayList<Product>();
+    public void addProduct(Product product) {
+        productList.add(product);
+        System.out.println("product added to chart = " + product);
+    }
 
 }
