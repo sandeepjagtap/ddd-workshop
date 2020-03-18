b9b9a2fcf2b54bb335eaa3bce7cb25712edc42f5
Sandeep Jagtap <sandeep.jagtap@gmail.com>
2020-03-18 12:22:42 +0530

    [Sandeep] Implemented Use case 6

--- ecommerce/src/main/com/tw/dddworkshop/application/Application.java
+++ ecommerce/src/main/com/tw/dddworkshop/application/Application.java
@@ -4,6 +4,8 @@ import com.tw.dddworkshop.domain.Cart;
 import com.tw.dddworkshop.domain.Item;
 import com.tw.dddworkshop.domain.Product;
 
+import javax.lang.model.SourceVersion;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -18,12 +20,21 @@ public class Application {
         cart.addItem(inkPenItem);
 
         Product bat = new Product("GM Cricket Bat");
-        Item batItem = new Item(bat, 1);
+        Item batItem = new Item(bat, 2);
         cart.addItem(batItem);
 
         Product ipadProToBeRemoved = new Product("Ipad Pro");
         Item ipadProToBeRemovedItem = new Item(ipadProToBeRemoved, 1);
         cart.removeItem(ipadProToBeRemovedItem);
+
+        //Use case 6
+        Cart cart1 = new Cart();
+
+        Cart cart2 = new Cart();
+
+        System.out.println("cart1 and cart 2 are different? = " + !cart1.equals(cart2));
+
+
 
     }
 }
--- ecommerce/src/main/com/tw/dddworkshop/domain/Cart.java
+++ ecommerce/src/main/com/tw/dddworkshop/domain/Cart.java
@@ -6,12 +6,20 @@ import com.tw.dddworkshop.domain.events.ItemRemovedFromCartEvent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 public class Cart {
+
+    private final UUID id;
 
     private List<DomainEvent>events = new ArrayList<>();
 
     private List<Item> items = new ArrayList<>();
+
+    public Cart() {
+      this.id = UUID.randomUUID();
+    }
 
     public void addItem(Item item) {
         items.add(item);
@@ -30,5 +38,18 @@ public class Cart {
         events.add(itemRemovedFromCartEvent);
 
         System.out.println("Cart now has "+ items);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cart cart = (Cart) o;
+        return id.equals(cart.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
